722ea8ce8b21d3ffb84e21ad7616ef2c5dedd554
Muhammad Anas <anasamin26@gmail.com>
2024-01-15 23:29:09 +0500

    Fourth Commit

--- client/src/components/SkeletonLoader.js
+++ client/src/components/SkeletonLoader.js
@@ -11,9 +11,10 @@ return(
         <svg class="w-10 h-80 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
         </svg>
     </div>
-<div role="status" class="flex items-center justify-center h-56 max-w-sm bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
-    <span class="sr-only">Loading...</span>
-</div>
+    <div class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+        <svg class="w-10 h-80 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+        </svg>
+    </div>
     <span class="sr-only">Loading...</span>
 </div>
 </>
