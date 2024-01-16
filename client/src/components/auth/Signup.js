353a121efca8c8d8f42d667f423a98a04350687f
Muhammad Anas <anasamin26@gmail.com>
2024-01-16 22:10:57 +0500

    Picture Updates

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -43,7 +43,7 @@ const Signup = () => {
           </div>
         </div>
 
-        <div className="flex justify-center self-center z-10 mb-16">
+        <div className="flex relative justify-center self-center z-10 mb-16">
           <div className="p-8 bg-white mx-auto rounded-3xl w-96">
             <div className="mb-7">
               <h3 className="font-semibold text-2xl text-gray-800">Register</h3>
@@ -149,10 +149,11 @@ const Signup = () => {
           </div>
         </div>
       </div>
-
+        <div className='sm:block hidden'>
       <svg className="absolute bottom-0 left-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#fff" fillOpacity="1" d="M0,0L40,42.7C80,85,160,171,240,197.3C320,224,400,192,480,154.7C560,117,640,75,720,74.7C800,75,880,117,960,154.7C1040,192,1120,224,1200,213.3C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
       </svg>
+      </div>
 
       <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"></script>
     </div>
